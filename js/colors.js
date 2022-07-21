7e9b38953752e557ecc2dfff352a999922d6f2de
jenifer9 <jeni9jacinto7p@gmail.com>
2022-07-21 17:36:49 -0600

    file js

---
+++ js/colors.js
@@ -0,0 +1,8 @@
+var btnchange = document.querySelector('button');
+var fondo = document.querySelector('body');
+var coloresb = ['blue', 'red', 'pink', 'yellow', 'aliceblue'];
+btnchange === null || btnchange === void 0 ? void 0 : btnchange.addEventListener('click', generarNuevoColor);
+function generarNuevoColor() {
+    var icolores = parseInt(Math.random() * coloresb.length);
+    fondo.style.backgroundColor = coloresb[icolores];
+}
---
+++ js/colors.ts
@@ -0,0 +1,11 @@
+var btnchange = document.querySelector('button');
+var fondo = document.querySelector('body');
+var coloresb: any = ['blue', 'red', 'pink', 'yellow','aliceblue'];
+
+btnchange?.addEventListener('click', generarNuevoColor);
+
+function generarNuevoColor(){
+	var  icolores = parseInt(Math.random()*coloresb.length);
+
+	fondo.style.backgroundColor = coloresb[icolores];
+}
